65145fb15cdfaf25f7710e7db7402b20d2bb1189
verebelyi.csaba <verebelyi.csaba@zengo.eu>
2021-10-16 14:35:56 +0200

    feat(blog): add author page

--- components/Author.vue
+++ components/Author.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
-    <img :src="author.image" />
+  <NuxtLink :to="`/blog/author/${author.name}`">
+    <img :src="author.img" />
     <div>
       <h4>Author</h4>
       <p>{{ author.name }}</p>
       <p>{{ author.bio }}</p>
     </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script>
-  export default {
-    props: {
-      author: {
-        type: Object,
-        required: true
-      }
-    }
-  }
+export default {
+  props: {
+    author: {
+      type: Object,
+      required: true,
+    },
+  },
+}
 </script>
