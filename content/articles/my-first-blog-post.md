92ea9256e920bd59ffae0c03839715ee0d860e43
verebelyi.csaba <verebelyi.csaba@zengo.eu>
2021-10-16 13:57:59 +0200

    feat(blog): md html + components

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -1,3 +1,41 @@
+---
+title: My first Blog Post
+description: Learning how to use @nuxt/content to create a blog
+img: first-blog-post.jpg
+alt: my first blog post
+author:
+  name: Csabi
+  bio: All about Csabi
+  image: https://images.unsplash.com/photo-1633794036121-e278381835a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNDM4NTE0Ng&ixlib=rb-1.2.1&q=80&w=1080
+---
+
 # My first blog post
 
 Welcome to my first blog post using content module
+
+## This is a heading
+
+This is some more info
+
+### This is a sub heading
+
+This is some more info
+
+### This is another sub heading
+
+This is some more info
+
+## This is another heading
+
+This is some more info
+
+<div class="p-4 mb-4 text-white bg-blue-500">
+  This is HTML inside markdown that has a class of note
+</div>
+
+
+<info-box>
+  <template #info-box>
+    This is a vue component inside markdown using slots
+  </template>
+</info-box>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,16 +1,36 @@
 <template>
   <article>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
+    <nav>
+      <ul>
+        <li v-for="link of article.toc" :key="link.id">
+          <NuxtLink :to="`#${link.id}`" :class="{ 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 }">{{ link.text }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+
     <nuxt-content :document="article" />
+
+    <author :author="article.author" />
   </article>
 </template>
 
 <script>
-  export default {
-    async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
 
-      return { article }
+    return { article }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
     }
-  }
+ }
+}
 </script>
-
