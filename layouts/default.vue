bf1aced97ef6dfe2ee99160828ff5976f2f4201f
Csaba Verebélyi <verebelyi.csaba@zengo.ez>
2021-10-17 12:50:05 +0200

    feat(ui): extract nav bar and blog layout

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="relative bg-white overflow-hidden">
+      <div class="max-w-7xl mx-auto">
+        <NavBar />
+
+        <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <Nuxt />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ layouts/hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="relative bg-white overflow-hidden">
+      <div class="max-w-7xl mx-auto">
+        <NavBar />
+      </div>
+    </div>
+
+    <Nuxt />
+  </div>
+</template>
